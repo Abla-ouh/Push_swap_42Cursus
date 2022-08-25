40bb746d54b36e17caa8f644b6cf8fa00f8b43ea
Abla-ouh <57642906+Abla-ouh@users.noreply.github.com>
2022-08-25 17:59:44 +0100

    Add files via upload

---
+++ push_swap/libft/ft_strtrim.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhaga <abouhaga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 20:56:57 by abouhaga          #+#    #+#             */
+/*   Updated: 2022/07/05 20:56:58 by abouhaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int	start;
+	int	end;
+	int	len;
+
+	if (!s1 || !set)
+		return (0);
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end >= 0 && ft_strchr(set, s1[end]))
+		end--;
+	len = end - start + 1;
+	if (len < 0)
+		return (ft_strdup(""));
+	return (ft_substr(s1, start, len));
+}
